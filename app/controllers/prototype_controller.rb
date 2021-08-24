d6b7c75bc508139534e8b18cfbbc309dc730f6af
runba3011 <runba3011@gmail.com>
2021-08-24 14:41:47 +0900

    ログイン機能

--- app/controllers/prototype_controller.rb
+++ app/controllers/prototype_controller.rb
@@ -1,4 +1,12 @@
 class PrototypeController < ApplicationController
+  before_action :set_user_variable
+  
   def index
   end
+
+  private
+  def set_user_variable
+    @user = User.new
+  end
+
 end
