9c7c873117fe1d5f9d2678ef7f02553ed6bb3383
runba3011 <runba3011@gmail.com>
2021-08-25 14:15:32 +0900

    空のコメントを投稿しようとしたときの処理の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,8 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype.id)
     else
-      @prototype = @comment.prototype
+      @prototype = Prototype.find(params[:prototype_id])
+      @comments = @prototype.comments.includes(:user)
       render "/prototypes/show"
     end
 
